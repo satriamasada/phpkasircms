4c1fe3a7abc5a2e9cbeb30d223e1b2bba26b5c00
mitrapersadapt <mitrasemestapersadapt@gmail.com>
2026-04-08 02:39:11 +0900

    logout redirect error

--- logout.php
+++ logout.php
@@ -1,10 +1,5 @@
 <?php
 session_start();
-// Auto backup on logout
-@include 'backup_db.php';
-if (function_exists('perform_db_backup')) {
-    perform_db_backup();
-}
 session_destroy();
 header('Location: login.php');
 exit;
